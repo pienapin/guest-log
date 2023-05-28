43e979a877ac22f61c19c8283b7484f2c81af60c
pien <pincenna@gmail.com>
2023-05-29 05:49:13 +0700

    feat: add router for admin pages

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import Home from "@/views/Home.vue";
+import AdminLayout from "@/layouts/AdminLayout.vue";
+import Dashboard from "@/views/admin/Dashboard.vue";
+import DaftarPengunjung from "@/views/admin/DaftarPengunjung.vue";
+import DaftarKunjungan from "@/views/admin/DaftarKunjungan.vue";
+import DaftarPengguna from "@/views/admin/DaftarPengguna.vue";
 
 const routes = [
   {
@@ -8,6 +13,45 @@ const routes = [
     path: "/",
     component: Home
   },
+  {
+    path: "/admin",
+    component: AdminLayout,
+    props: (route) => {
+      return {
+        title: route.meta.title
+      }
+    },
+    children: [
+      {
+        path: "",
+        component: Dashboard,
+        meta: {
+          title: "Dashboard",
+        },
+      },
+      {
+        path: "daftar-pengunjung",
+        component: DaftarPengunjung,
+        meta: {
+          title: "Daftar Pengunjung",
+        },
+      },
+      {
+        path: "daftar-kunjungan",
+        component: DaftarKunjungan,
+        meta: {
+          title: "Daftar Kunjungan",
+        },
+      },
+      {
+        path: "daftar-pengguna",
+        component: DaftarPengguna,
+        meta: {
+          title: "Daftar Pengguna",
+        },
+      }
+    ],
+  }
 ];
 
 const router = createRouter({
