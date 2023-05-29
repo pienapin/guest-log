46eb6ef7038cd621cbf99c3d340fe0920131c83e
Alvien Cenna <pincenna@gmail.com>
2023-05-29 13:43:23 +0700

    feat: add pagination to Daftar Kunjungan table

--- frontend/src/views/admin/DaftarKunjungan.vue
+++ frontend/src/views/admin/DaftarKunjungan.vue
@@ -11,42 +11,48 @@
         <th class="text-start">Tujuan</th>
         <th>Action</th>
       </tr>
-      <tr v-for="(kunjungan, index) in kunjunganList" class="hover text-center">
-        <div class="hidden">
-          {{ pengunjung = kunjungan.pengunjung }}
-          {{ kategori = kunjungan.kategori }}
-        </div>
-        <th>{{ index + 1 }}</th>
-        <td class="text-start">{{ pengunjung.nama }}</td>
-        <td>{{ pengunjung.jabatan }} di {{ pengunjung.instansi }}</td>
-        <td>{{ pengunjung.no_hp }}</td>
-        <td>{{ pengunjung.no_wa }}</td>
-        <td>{{ kategori.kategori }}</td>
-        <td class="text-justify whitespace-normal">{{ kunjungan.tujuan }}</td>
-        <td class="text-center">
-          <label :for="'hapus_' + kunjungan.id" @click="stopPolling"
-            class="btn mx-1 btn-error text-base-100 btn-square min-h-0 h-8 w-8 p-0 text-xs">
-            <i class="fa-solid fa-trash"></i>
-          </label>
-          <HapusKunjungan :poll="startPolling" :kunjungan="kunjungan"></HapusKunjungan>
-        </td>
-      </tr>
+        <tr v-if="kunjunganList" v-for="(kunjungan, index) in kunjunganList.data" class="hover text-center">
+          <div class="hidden">
+            {{ pengunjung = kunjungan.pengunjung }}
+            {{ kategori = kunjungan.kategori }}
+          </div>
+          <th>{{ index + 1 }}</th>
+          <td class="text-start">{{ pengunjung.nama }}</td>
+          <td>{{ pengunjung.jabatan }} di {{ pengunjung.instansi }}</td>
+          <td>{{ pengunjung.no_hp }}</td>
+          <td>{{ pengunjung.no_wa }}</td>
+          <td>{{ kategori.kategori }}</td>
+          <td class="text-justify whitespace-normal">{{ kunjungan.tujuan }}</td>
+          <td class="text-center">
+            <label :for="'hapus_' + kunjungan.id" @click="stopPolling"
+              class="btn mx-1 btn-error text-base-100 btn-square min-h-0 h-8 w-8 p-0 text-xs">
+              <i class="fa-solid fa-trash"></i>
+            </label>
+            <HapusKunjungan :poll="startPolling" :kunjungan="kunjungan"></HapusKunjungan>
+          </td>
+        </tr>
     </tbody>
   </table>
+  <div v-if="kunjunganList" class="btn-group mt-5">
+    <button v-if="kunjunganList.prev_page_url" class="btn" @click="currentPage -= 1">«</button>
+    <button class="btn">Page {{ kunjunganList.current_page }} of {{ kunjunganList.last_page }}</button>
+    <button v-if="kunjunganList.next_page_url" class="btn" @click="currentPage += 1">»</button>
+  </div>
 </template>
 
 <script setup>
 /* ==================== imports ==================== */
-import { onBeforeUnmount, onMounted, onUpdated, ref } from 'vue';
-import { getKunjunganList } from '../../services/kunjungan';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { getKunjunganPage } from '../../services/kunjungan';
 import HapusKunjungan from '../../components/HapusKunjungan.vue';
 
 let intervalId;
 let kunjunganList;
 const renderCount = ref(0);
+const currentPage = ref(1);
 
 const fetchData = async () => {
-  kunjunganList = await getKunjunganList();
+  kunjunganList = await getKunjunganPage(currentPage.value);
   console.log(kunjunganList)
   renderCount.value += 1;
 }
