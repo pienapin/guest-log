e9e083381518aea46bf14bc7eca01de118f824b4
Alvien Cenna <pincenna@gmail.com>
2023-05-29 09:22:01 +0700

    feat: slice DaftarKunjungan table

--- frontend/src/views/admin/DaftarKunjungan.vue
+++ frontend/src/views/admin/DaftarKunjungan.vue
@@ -1,7 +1,75 @@
 <template>
-  
+  <table :key="renderCount" data-theme="light" class="table table-compact rounded-lg w-full">
+    <tbody>
+      <tr class="text-center">
+        <th>No</th>
+        <th class="text-start">Nama</th>
+        <th>Jabatan dan Instansi</th>
+        <th>No. HP</th>
+        <th>No. WA</th>
+        <th>Kategori</th>
+        <th class="text-start">Tujuan</th>
+        <th>Action</th>
+      </tr>
+      <tr v-for="(kunjungan, index) in kunjunganList" class="hover text-center">
+        <div class="hidden">
+          {{ pengunjung = kunjungan.pengunjung }}
+          {{ kategori = kunjungan.kategori }}
+        </div>
+        <th>{{ index + 1 }}</th>
+        <td class="text-start">{{ pengunjung.nama }}</td>
+        <td>{{ pengunjung.jabatan }} di {{ pengunjung.instansi }}</td>
+        <td>{{ pengunjung.no_hp }}</td>
+        <td>{{ pengunjung.no_wa }}</td>
+        <td>{{ kategori.kategori }}</td>
+        <td class="text-justify whitespace-normal">{{ kunjungan.tujuan }}</td>
+        <td class="text-center">
+          <label :for="'hapus_' + kunjungan.id"
+            class="btn mx-1 btn-error text-base-100 btn-square min-h-0 h-8 w-8 p-0 text-xs">
+            <i class="fa-solid fa-trash"></i>
+          </label>
+          <!-- <HapusPengunjung :pengunjung="pengunjung"></HapusPengunjung> -->
+        </td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script setup>
+/* ==================== imports ==================== */
+import { onBeforeUnmount, onMounted, ref } from 'vue';
+import { getKunjunganList } from '../../services/kunjungan';
 
+let intervalId;
+let kunjunganList;
+const renderCount = ref(0);
+
+const fetchData = async () => {
+  kunjunganList = await getKunjunganList();
+  console.log(kunjunganList)
+}
+
+onMounted(() =>{
+  startPolling();
+});
+
+onBeforeUnmount(() => {
+  stopPolling();
+});
+
+
+/* ==================== functions ==================== */
+const startPolling = async () => {
+  await fetchData();
+  renderCount.value += 1;
+  intervalId = setInterval(() => {
+    fetchData();
+    renderCount.value += 1;
+  }, 5000);
+}
+
+
+const stopPolling = () => {
+  clearInterval(intervalId);
+}
 </script>
