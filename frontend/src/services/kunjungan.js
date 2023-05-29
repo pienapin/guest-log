577325534224961e70d656fc56af4c08ee9aaaf8
Alvien Cenna <pincenna@gmail.com>
2023-05-29 13:42:48 +0700

    feat: add service for kunjunganList with page

--- frontend/src/services/kunjungan.js
+++ frontend/src/services/kunjungan.js
@@ -9,6 +9,13 @@ export async function getKunjunganList() {
   return response.data;
 }
 
+export async function getKunjunganPage(page) {
+  const response = await baseApi
+    .get(`${api}?perPage=15&paginate=1&page=${page}`);
+    
+  return response.data;
+}
+
 export async function addKunjungan(body) {
   const response = await baseApi
     .post(`${api}/submit`, body);
