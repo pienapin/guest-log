bba58498e1044c9bf1261dab5df2447502dd1b31
Alvien Cenna <pincenna@gmail.com>
2023-05-30 08:42:27 +0700

    feat: add service for search on pengunjung

--- frontend/src/services/pengunjung.js
+++ frontend/src/services/pengunjung.js
@@ -16,6 +16,20 @@ export async function getPengunjungPage(page) {
   return response.data;
 }
 
+export async function searchPengunjung(body, page) {
+  let params = ""
+  if (body.keyword) params += `&keyword=${body.keyword}`
+  if (body.instansi) params += `&instansi=${body.instansi}`
+  if (body.jabatan) params += `&jabatan=${body.jabatan}`
+  if (body.email) params += `&email=${body.email}`
+  if (body.no_wa) params += `&no_wa=${body.no_wa}`
+  if (body.no_hp) params += `&no_hp=${body.no_hp}`
+  const response = await baseApi
+    .get(`${api}?perPage=15&paginate=1\&page=${page}${params}`)
+  
+  return response.data
+}
+
 export async function addPengunjung(body) {
   const response = await baseApi
     .post(`${api}/submit`, body);
