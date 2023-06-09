44a1c54af2c2018dea372ab9f4ea0184c7c7270d
Alvien Cenna <pincenna@gmail.com>
2023-06-09 07:28:22 +0700

    chore: use pinia on app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,12 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+import store from "./stores"
 import './style.css'
 import App from './App.vue'
 import router from './routes'
 
 createApp(App)
+  .use(createPinia())
+  .use(store)
   .use(router)
   .mount('#app')
