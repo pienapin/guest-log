75cfc583d80a7066afe2a721b735c37ae089eb35
Alvien Cenna <pincenna@gmail.com>
2023-06-09 23:09:47 +0700

    refactor: follow things on internet to use pinia

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import store from "./stores"
 import './style.css'
 import App from './App.vue'
 import router from './routes'
 
-createApp(App)
-  .use(createPinia())
-  .use(store)
-  .use(router)
-  .mount('#app')
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+app.use(router)
+app.mount('#app')
