8ee5f37fc9aecb8890dc2627134558c5af2650ac
Alvien Cenna <pincenna@gmail.com>
2023-06-09 23:11:18 +0700

    feat: add Login and router guards

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import { certCookies } from "@/plugins/cookies";
 import Home from "@/views/Home.vue";
 import AdminLayout from "@/layouts/AdminLayout.vue";
 import Dashboard from "@/views/admin/Dashboard.vue";
@@ -7,12 +7,21 @@ import DaftarPengunjung from "@/views/admin/DaftarPengunjung.vue";
 import DaftarKunjungan from "@/views/admin/DaftarKunjungan.vue";
 import DaftarPengguna from "@/views/admin/DaftarPengguna.vue";
 import DaftarPelayanan from "@/views/admin/DaftarPelayanan.vue";
+import Login from '@/views/auth/Login.vue'
+import useAuthStore from "../stores/auth";
 
 const routes = [
   {
     name: "Home",
     path: "/",
-    component: Home
+    component: Home,
+    meta: { guest: true }
+  },
+  {
+    name: "Login",
+    path: "/login",
+    component: Login,
+    meta: { guest: true }
   },
   {
     path: "/admin",
@@ -25,6 +34,7 @@ const routes = [
     children: [
       {
         path: "",
+        name: "admin-dashboard",
         component: Dashboard,
         meta: {
           title: "Dashboard",
@@ -59,12 +69,34 @@ const routes = [
         },
       },
     ],
+    meta: { auth: true, admin: true }
   }
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.beforeEach(async (to, from, next) => {
+  const authStore = useAuthStore()
+  const user = await authStore.setUser();
+  console.log(user)
+  if (to.meta.auth) {
+    if (!user) next({ name: 'Login'});
+    if (to.meta.admin && user.role_id == 1) next();
+    if (to.meta.kepala && user.role_id == 2) next();
+    if (to.meta.pst && user.role_id == 3) next();
+    // next({ name: 'Unauthorized' });
+  } else if (to.meta.guest) {
+    if (!user) next();
+    else {
+      if (user.role_id == 1) next({ name: 'admin-dashboard' });
+      if (user.role_id == 2) next();
+      if (user.role_id == 3) next();
+    }
+  }
+  // next()
 });
 
 export default router;
