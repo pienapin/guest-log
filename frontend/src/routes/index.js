544fd8fb4817f9ee969636e02da00a19836beb11
Alvien Cenna <pincenna@gmail.com>
2023-06-26 09:46:12 +0700

    fix: change to webHashHistory for non-node server

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { certCookies } from "@/plugins/cookies";
 import Home from "@/views/Home.vue";
 import AdminLayout from "@/layouts/AdminLayout.vue";
@@ -173,7 +173,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 });
 
